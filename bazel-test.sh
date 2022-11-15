d51bef29364244ef3b2f6be59781db08c1dc8ce2
dalonso-gz <daniel.alonso@tink.com>
2022-11-15 14:10:29 +0100

    test2

--- bazel-test.sh
+++ bazel-test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-docker run \
-  -e USER="$(id -u)" \
-  -u="$(id -u)" \
-  -v ~/bazel-hello:/src/bazel-hello \
-  -v ~/tmp/build_output:/tmp/build_output \
-  -w /src/bazel-hello \
-  l.gcr.io/google/bazel:latest \
-  --output_user_root=~/tmp/build_output \
-  run //src/main/java/se/tink/hello:docker
+
+bazel run //src/main/java/se/tink/hello:hello
+
+# docker run \
+#   -e USER="$(id -u)" \
+#   -u="$(id -u)" \
+#   -v ~/bazel-hello:/src/bazel-hello \
+#   -v ~/tmp/build_output:/tmp/build_output \
+#   -w /src/bazel-hello \
+#   l.gcr.io/google/bazel:latest \
+#   --output_user_root=~/tmp/build_output \
+#   run //src/main/java/se/tink/hello:docker
